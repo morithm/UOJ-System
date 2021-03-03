76299e972664f74bb8f5a241eeacfc9f52323545
morithm <wj@morithm.com>
2021-03-03 22:10:05 +0000

    add auth check to route

---
+++ web/app/controllers/need_login.php
@@ -0,0 +1,9 @@
+<?php
+?>
+<?php echoUOJPageHeader(UOJLocale::get('login')) ?>
+<div class="container">
+    <p>请先登录</p>
+
+    <button onclick="location.href='/login'" type="button" class="btn btn-outline-secondary">确定</button>
+</div>
+<?php echoUOJPageFooter() ?>
